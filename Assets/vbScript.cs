7023ba89e7ed87a4aa9f759305fba6dc24a9c91d
Abay Abay <Abay Abay>
2017-04-02 18:36:03 +0100

    created AR project

---
+++ Assets/vbScript.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Vuforia;
+
+public class vbScript : MonoBehaviour, IVirtualButtonEventHandler {
+
+    private GameObject vbButtonObject;
+    private GameObject zombie;
+	// Use this for initialization
+	void Start () {
+        vbButtonObject = GameObject.Find("actionButton");
+        zombie = GameObject.Find("zombie");
+        vbButtonObject.GetComponent<VirtualButtonBehaviour>().RegisterEventHandler(this);
+	}
+    public void OnButtonPressed(VirtualButtonAbstractBehaviour vb) {
+        Debug.Log("Button down!!!");
+        zombie.GetComponent<Animation>().Play();
+    }
+
+    public void OnButtonReleased(VirtualButtonAbstractBehaviour vb)
+    {
+        Debug.Log("Button released!!!");
+        zombie.GetComponent<Animation>().Stop();
+    }
+
+}
